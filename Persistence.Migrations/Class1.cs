82962bd37a60a42d834f3fd6348f2d27fad22087
Fereshte Saeedi <f.saeedi@sadad.co.ir>
2020-04-06 12:03:27 +0430

    alter proj

--- Persistence.Migrations/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Persistence.Migrations
-{
-    public class Class1
-    {
-    }
-}
--- PersonWebApp/Program.cs
+++ PersonWebApp/Program.cs
@@ -20,7 +20,7 @@ namespace PersonWebApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseIISIntegration().UseStartup<Startup>();
                 }).UseServiceProviderFactory(new DryIocServiceProviderFactory()).ConfigureContainer<Container>((hostContext, container)=>{
                     container.RegisterByAssemblyAndType<IService>("Services");
                     container.RegisterByAssemblyAndType<IRepository, IService>("Persistence.Dapper");
